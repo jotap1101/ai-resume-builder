5398762d35cddce1d94f1ff76cb7a41acce45b58
jotap1101 <jotap1101.joaopedro@gmail.com>
2026-01-03 08:22:29 -0300

    feat: add eslint-plugin-simple-import-sort for improved import sorting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+    plugins: ["simple-import-sort", "import"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
